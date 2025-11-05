fc6adad062b7525815e68411a4ce497be79ed9dc
meerae-shin <meerae.shin@causeworks.co.kr>
2025-11-05 16:46:21 +0900

    tailwind 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,48 +2,53 @@ import { useState } from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 import Contact from './Contact'
 
-function Home() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>포트폴리오</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Link className="read-the-docs" to="/contact">
-        컨텍페이지
-      </Link>
-    </>
-  )
+function Home() {
+    const [count, setCount] = useState(0)
+    return (
+        <>
+            <section>
+                <div className="inner">
+                    <a href="https://vite.dev" target="_blank" rel="noreferrer">
+                        <img src={viteLogo} className="logo" alt="Vite logo" />
+                    </a>
+                    <a href="https://react.dev" target="_blank" rel="noreferrer">
+                        <img src={reactLogo} className="logo react" alt="React logo" />
+                    </a>
+                </div>
+            </section>
+            <section>
+                <h1 className="text-3xl font-bold text-blue-200">포트폴리오</h1>
+                <h1 className="text-3xl font-bold text-main-blue">포트폴리오</h1>
+                <div className="card">
+                    <button onClick={() => setCount((count) => count + 1)}>
+                        count is {count}
+                    </button>
+                    <p>
+                        Edit <code>src/App.jsx</code> and save to test HMR
+                    </p>
+                </div>
+                <p className="read-the-docs">
+                    Click on the Vite and React logos to learn more
+                </p>
+                <Link className="read-the-docs" to="/contact">
+                    컨텍페이지
+                </Link>
+            </section>
+        </>
+    )
 }
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
-  )
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+        </Routes>
+    )
 }
 
 export default App
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'main-blue': '#3490dc',
+        'point-pink': '#ff69b4',
+      },
+    },
+  },
+  plugins: [],
+};
